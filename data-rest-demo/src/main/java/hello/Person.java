ce8492b803d12ecb8f9c06a5f33770acb8bd0f7a
Carsten Gräf <evandor@gmail.com>
2018-03-28 09:10:28 +0200

    wip

--- data-rest-demo/src/main/java/hello/Person.java
+++ data-rest-demo/src/main/java/hello/Person.java
@@ -1,7 +1,5 @@
 package hello;
 
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,15 +7,38 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
 public class Person {
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	@Id
+
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
 	private String firstName;
+
 	private String lastName;
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
