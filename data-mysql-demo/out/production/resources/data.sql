06295733e9bf0cdb422a3a632b587bfd02f2c402
evandor <evandor@gmail.com>
2018-04-09 11:20:58 +0200

    cleanup

---
+++ data-mysql-demo/out/production/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO monitor (id, name, url) VALUES (1,'skysail test demo', 'http://demo.test.skysail.io');
+INSERT INTO monitor (id, name, url) VALUES (2,'skysail int demo', 'http://demo.int.skysail.io');
--- skysail-services-monitor/out/production/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO monitor (id, email, name, url) VALUES (1, 'evandor@gmail.com', 'skysail test demo', 'http://demo.test.skysail.io');
-INSERT INTO monitor (id, email, name, url) VALUES (2, 'evandor@gmail.com', 'skysail int demo', 'http://demo.int.skysail.io');
