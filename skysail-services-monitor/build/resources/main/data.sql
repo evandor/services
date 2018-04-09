d04f76d1070681bb3f41ad7f17f83fd27e4bed87
evandor <evandor@gmail.com>
2018-04-09 14:59:51 +0200

    wip

--- skysail-services-monitor/build/resources/main/data.sql
+++ skysail-services-monitor/build/resources/main/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO monitor (id, email, name, url) VALUES (1, 'evandor@gmail.com', 'skysail test demo', 'http://demo.test.skysail.io');
-INSERT INTO monitor (id, email, name, url) VALUES (2, 'evandor@gmail.com', 'skysail int demo', 'http://demo.int.skysail.io');
+INSERT INTO monitor (id, email, name, url)
+  VALUES (1, 'evandor@gmail.com', 'skysail test demo', 'http://demo.test.skysail.io');
+INSERT INTO monitor (id, email, name, url)
+  VALUES (2, 'evandor@gmail.com', 'skysail int demo', 'http://demo.int.skysail.io');
